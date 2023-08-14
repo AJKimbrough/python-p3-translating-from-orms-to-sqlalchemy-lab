c8eb60c581bfdce2554a8f19bd7d431e8706a877
Amber Kimbrough (AJ) <94200525+AJKimbrough@users.noreply.github.com>
2023-08-14 14:14:56 -0500

    complete lab

--- lib/models.py
+++ lib/models.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import (Column, String, Integer)
+from sqlalchemy import (Column, String, Integer, Index)
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 class Dog(Base):
     __tablename__ = 'dogs'
+    Index('index_name', 'name')
 
     id = Column(Integer(), primary_key=True)
     name = Column(String())
